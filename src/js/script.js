cc37ba3fa9bbc1963453347f7883403d4434661d
Mariano0o92 <mariuszn13@gmail.com>
2023-10-26 15:25:40 +0200

    Standings table for each league fully done

--- src/js/script.js
+++ src/js/script.js
@@ -1,29 +1,29 @@
 // ALL DATA
 const heroImg = document.querySelector('.hero-img')
 const hamburger = document.querySelector('.hamburger')
-const nav = document.querySelector(".nav");
+const nav = document.querySelector('.nav')
 const mobileLink = document.querySelectorAll('.nav-mobile__link')
 const mobileLinks = document.querySelector('.nav-mobile__links')
 const navLinks = document.querySelectorAll('.nav__link')
-const navResults = document.querySelectorAll('.nav__results');
-const navTable = document.querySelectorAll('.nav__table a');
+const navResults = document.querySelectorAll('.nav__results')
+const navTable = document.querySelectorAll('.nav__table a')
 const sections = document.querySelectorAll('.main')
-const tables = document.querySelectorAll('.table');
+const tables = document.querySelectorAll('.table')
 
-// Function to set the active section based on the URL hash
+
 function setActiveSectionFromHash() {
-	const hash = window.location.hash.substring(1); // Remove the '#' character
-	const section = document.getElementById(hash);
-	
+	const hash = window.location.hash.substring(1)
+	const section = document.getElementById(hash)
+
 	if (section) {
-	  removeActiveClasses(); // Remove active classes from all sections
-	  section.classList.add('main__active'); // Add active class to the section with the hash
+		removeActiveClasses()
+		section.classList.add('main__active')
 	}
-  }
-  
-  // Add event listener to handle hash changes and initial page load
-  window.addEventListener('hashchange', setActiveSectionFromHash);
-  window.addEventListener('load', setActiveSectionFromHash);
+}
+
+
+window.addEventListener('hashchange', setActiveSectionFromHash)
+window.addEventListener('load', setActiveSectionFromHash)
 
 // HERO IMAGE
 
@@ -42,166 +42,192 @@ hamburger.addEventListener('click', () => {
 })
 
 
-  // Add click event listeners to .nav__link elements
-  navLinks.forEach((link) => {
-    link.addEventListener("click", () => {
-      // Remove previously added divs (if any)
-      const existingResultsDiv = nav.querySelector(".nav__results");
-      const existingTableDiv = nav.querySelector(".nav__table");
-      if (existingResultsDiv) {
-        existingResultsDiv.remove();
-      }
-      if (existingTableDiv) {
-        existingTableDiv.remove();
-      }
-	  
-      // Create Results and Table links for the clicked .nav__link
-      const linkHref = link.getAttribute("href");
-	  
-      const resultsLink = document.createElement("a");
-      resultsLink.href = linkHref;
-      resultsLink.textContent = "Results";
+navLinks.forEach(link => {
+	link.addEventListener('click', () => {
 
-      const tableLink = document.createElement("a");
-      tableLink.href = linkHref + "-table";
-      tableLink.textContent = "Table";
-	  
-      const resultsDiv = document.createElement("div");
-      resultsDiv.className = "nav__results nav__results--active";
-      resultsDiv.appendChild(resultsLink);
-	  
-      const tableDiv = document.createElement("div");
-      tableDiv.className = "nav__table";
-      tableDiv.appendChild(tableLink);
-	  
-      // Append Results and Table divs to the .nav container
-      nav.appendChild(resultsDiv);
-      nav.appendChild(tableDiv);
-    });
-});
+		const existingResultsDiv = nav.querySelector('.nav__results')
+		const existingTableDiv = nav.querySelector('.nav__table')
+		if (existingResultsDiv) {
+			existingResultsDiv.remove()
+		}
+		if (existingTableDiv) {
+			existingTableDiv.remove()
+		}
+
+	
+		const linkHref = link.getAttribute('href')
+
+		const resultsLink = document.createElement('a')
+		resultsLink.href = linkHref
+		resultsLink.textContent = 'Results'
+
+		const tableLink = document.createElement('a')
+		tableLink.href = linkHref + '-table'
+		tableLink.textContent = 'Standings'
+
+		const resultsDiv = document.createElement('div')
+		resultsDiv.className = 'nav__results nav__results--active'
+		resultsDiv.appendChild(resultsLink)
+
+		const tableDiv = document.createElement('div')
+		tableDiv.className = 'nav__table'
+		tableDiv.appendChild(tableLink)
+
+		
+		nav.appendChild(resultsDiv)
+		nav.appendChild(tableDiv)
+	})
+})
 
 // MAIN SECTION
 
 // Toggle class of __active
+
+// DESKTOP
+
 navLinks.forEach((link, index) => {
 	link.addEventListener('click', () => {
-		const isActive = link.classList.contains('nav__link--active');
-    
-		// If it's not active, remove active classes from other links and add the active class
+		const isActive = link.classList.contains('nav__link--active')
+
+		
 		if (!isActive) {
-		  removeActiveClasses();
-		  sections[index].classList.add('main__active');
-		  // Also remove nav__link--active from other links
-		  navLinks.forEach((navLink) => {
-			navLink.classList.remove('nav__link--active');
-		  });
-		  link.classList.add('nav__link--active');
+			removeActiveClasses()
+			sections[index].classList.add('main__active')
+			
+			navLinks.forEach(navLink => {
+				navLink.classList.remove('nav__link--active')
+			})
+			link.classList.add('nav__link--active')
 		}
 
-			navLinks.forEach((navLink) => {
-				navLink.classList.remove('nav__link--active');
-			  });
-			  
-			  link.classList.add('nav__link--active');
-
+		navLinks.forEach(navLink => {
+			navLink.classList.remove('nav__link--active')
 		})
-    })
+
+		link.classList.add('nav__link--active')
+	})
+})
+
+// MOBILE
+
 mobileLink.forEach((link, index) => {
 	link.addEventListener('click', () => {
-		const isActive = link.classList.contains('nav__link--active');
-    
-		// If it's not active, remove active classes from other links and add the active class
+		const isActive = link.classList.contains('nav-mobile__link--active')
+
 		if (!isActive) {
-		  removeActiveClasses();
-		  sections[index].classList.add('main__active');
-		  // Also remove nav__link--active from other links
-		  navLinks.forEach((navLink) => {
-			navLink.classList.remove('nav__link--active');
-		  });
-		  link.classList.add('nav__link--active');
+			removeActiveClasses()
+			sections[index].classList.add('main__active')
+	
+			mobileLink.forEach(mobileLink => {
+				mobileLink.classList.remove('nav-mobile__link--active')
+			})
+			link.classList.add('nav-mobile__link--active')
 		}
 
-			mobileLink.forEach((mobileLink) => {
-				mobileLink.classList.remove('nav-mobile__link--active');
-			  });
+		mobileLink.forEach(mobileLink => {
+			mobileLink.classList.remove('nav-mobile__link--active')
+		})
 
-			  link.classList.add('nav-mobile__link--active');
+		link.classList.add('nav-mobile__link--active')
 
-			mobileLinks.classList.toggle('nav-mobile__links--active')
-			hamburger.classList.toggle('is-active')
-		});
-    })
+		mobileLinks.classList.toggle('nav-mobile__links--active')
+		hamburger.classList.toggle('is-active')
+	})
+})
 
-	navTable.forEach((link, index) => {
-		link.addEventListener('click', (event) => {
-		  event.preventDefault();
-		  removeActiveClasses();
-		  sections[index + 1].classList.add('main__active'); // +1 to select the next section
-		});
-	  })
+navTable.forEach((link, index) => {
+	link.addEventListener('click', event => {
+		event.preventDefault()
+		removeActiveClasses()
+		sections[index + 1].classList.add('main__active') // +1 to select the next section
+	})
+})
 
 function removeActiveClasses() {
 	sections.forEach(section => {
 		section.classList.remove('main__active')
-    })
+	})
 }
-
 
 // API DATA
 
 let leagues = ['PL', 'PD', 'BL1', 'SA', 'FL1']
 
 leagues.forEach((id, index) => {
-	// getData(id, index)
-
-});
+	getData(id, index)
+})
 
 function getData(id, pageIndex) {
 	const URL_API = 'https://api.football-data.org/v4/competitions/'
 	const leagueID = `${id}`
 
-
 	fetch(URL_API + leagueID + '/standings', {
 		method: 'GET',
 		headers: {
 			'X-Auth-Token': '268640ec6a2640e7991cee35b5c601b6',
-			'Accept-Encoding': ''
+			'Accept-Encoding': '',
 		},
 	})
 		.then(res => res.json())
 		.then(data => {
 			createstandings(data, pageIndex)
-			console.log(data);
 		})
 		.catch(err => {
 			console.log(err)
 		})
-	}
-
+}
 
 // 	const createScores = (league, pageIndex) => {
 // 		const sectionID = ['english', 'spanish', 'german', 'italian', 'french'][pageIndex];
 // 		const scores = document.getElementById(sectionID).querySelector('.scores')
-		
-		
-		
+
 // };
-	const createstandings = (league, pageIndex) => {
-		const sectionID = ['english-table', 'spanish-table', 'german-table', 'italian-table', 'french-table'][pageIndex];
-		const standings = document.getElementById(sectionID).querySelector('.standings')
-		standings.innerHTML = `<thead class="standings__header">
+const createstandings = (league, pageIndex) => {
+	const sectionID = ['english-table', 'spanish-table', 'german-table', 'italian-table', 'french-table'][pageIndex]
+	const standings = document.getElementById(sectionID).querySelector('.standings')
+	const getTable = league.standings[0].table
+
+	  const header = document.createElement('thead');
+	  header.classList.add('standings__header');
+	  header.innerHTML = `
 		<tr>
-		  <th class="rank">#</th>
-		  <th class="team">Team</th>
-		  <th class="played">P</th>
-		  <th class="won">W</th>
-		  <th class="drawn">D</th>
-		  <th class="lost">L</th>
-		  <th class="for">+</th>
-		  <th class="against">-</th>
-		  <th class="difference">+/-</th>
-		  <th class="points">P</th>
+		  <th class="standings__header-rank">#</th>
+		  <th class="standings__header-team">Team</th>
+		  <th class="standings__header-played">P</th>
+		  <th class="standings__header-won">W</th>
+		  <th class="standings__header-drawn">D</th>
+		  <th class="standings__header-lost">L</th>
+		  <th class="standings__header-for">+</th>
+		  <th class="standings__header-against">-</th>
+		  <th class="standings__header-difference">+/-</th>
+		  <th class="standings__header-points">P</th>
 		</tr>
-	  </thead>
-	  <tbody class="standings__body"></tbody>`
-};
+	  `;
+	  standings.appendChild(header);
+	
+	  const tbody = document.createElement('tbody');
+	  tbody.classList.add('standings__teams');
+
+	  getTable.forEach((teamData) => {
+		const tr = document.createElement('tr');
+		tr.innerHTML = `
+		  <td class="standings__teams-rank">${teamData.position}</td>
+		  <td class="standings__teams-team">
+			<img src="${teamData.team.crest}" alt="${teamData.team.name}">
+			<span class="standings__teams-name-full">${teamData.team.name}</span>
+		  </td>
+		  <td class="standings__teams-played">${teamData.playedGames}</td>
+		  <td class="standings__teams-won">${teamData.won}</td>
+		  <td class="standings__teams-drawn">${teamData.draw}</td>
+		  <td class="standings__teams-lost">${teamData.lost}</td>
+		  <td class="standings__teams-for">${teamData.goalsFor}</td>
+		  <td class="standings__teams-against">${teamData.goalsAgainst}</td>
+		  <td class="standings__teams-difference">${teamData.goalDifference}</td>
+		  <td class="standings__teams-points">${teamData.points}</td>
+		`;
+		tbody.appendChild(tr);
+	  });
+
+	  standings.appendChild(tbody)
+
+}
